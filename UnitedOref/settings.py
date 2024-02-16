c803947186f827e4fa7db44fa24f4280f7d1fb5b
Lior Cohen <lior32166@gmail.com>
2024-02-16 17:43:48 +0200

    Turned off debug mode

--- UnitedOref/settings.py
+++ UnitedOref/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u_h7)lyy+=jbcl6*nr@%s3v+l-fq=6zq137-ak#twur31!2j5!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
