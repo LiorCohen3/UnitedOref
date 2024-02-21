8570eb61b127e6dd990c8b17fa91e808519c478f
Lior Cohen <lior32166@gmail.com>
2024-02-21 22:39:22 +0200

    Updated views.py

--- main/views.py
+++ main/views.py
@@ -4,46 +4,51 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import requests
+
+
 # from .Algorithm import alg
 
 
-
-#@login_required()
+# @login_required()
 def say_hello(request):
     return HttpResponse("Hello World")
 
-#@login_required()
+
+# @login_required()
 def index(request):
     requests_list = requests.objects.filter(requests_status_id=2).order_by('date')
     # filtered_requests = alg(filtered_requests)
     area = "North"
     type = 1
-    algScore={}
-    for index,request in enumerate(requests_list):
-        algScore[index]=1
-        if request.area == area:  #same area donation
-            algScore[index]=algScore[index]+10
-        elif (area=="North" and request.area=="South") or (area=="South" and request.area=="North"):   #far area donation
-            algScore[index]=algScore[index]+1
-        else:   #semi far area donation
-            algScore[index]=algScore[index]+5
+    algScore = {}
+    for index, req in enumerate(requests_list):
+        algScore[index] = 1
+        if req.area == area:  # same area donation
+            algScore[index] = algScore[index] + 10
+        elif (area == "North" and req.area == "South") or (
+                area == "South" and req.area == "North"):  # far area donation
+            algScore[index] = algScore[index] + 1
+        else:  # semi far area donation
+            algScore[index] = algScore[index] + 5
 
-        if request.type_id==type: #type of donation matchs
-            algScore[index]=algScore[index]+10
+        if req.type_id == type:  # type of donation matchs
+            algScore[index] = algScore[index] + 10
         # if request.type_id.quantityReq<=donationCap:    #he can donate more then the req
         #     algScore[index]+=5*(donationCap/request.type_id.quantityReq)
         # else:   #the amount he can is lower then the req
         #     algScore[index]-=30
 
-        algScore[index]=algScore[index]+(10/(index+1))    #add score by the index for the request
-    sorted_list = {}
-    sorted_list = sorted(requests_list, key=lambda x: x.algScore, reverse=True) #order list by highest algScore
-    return render(request, 'requestList.html', {'requests': sorted_list})
-    #return render(request, 'index.html')
+        algScore[index] = algScore[index] + (10 / (index + 1))  # add score by the index for the request
+    sorted_keys = list(sorted(algScore, key=lambda x: algScore[x], reverse=True))
+    sorted_requests = [requests_list[i] for i in sorted_keys]
+    return render(request, 'requestList.html', {'requests': sorted_requests})
+    # return render(request, 'index.html')
 
-#@login_required()
+
+# @login_required()
 def dashboard(request):
     return render(request, 'dashboard.html')
+
 
 def request_list(request):
     return render(request, 'requesrList.html')
