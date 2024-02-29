bbbe60524b3d59433e689342e3face56f3d3f6b5
Lior Cohen <lior32166@gmail.com>
2024-02-29 23:58:09 +0200

    Removed redundant url path

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('method/', views.donation_type, name='Donation Type'),
     path('pending/', views.pending, name='Pending'),
     path('edit_profile/', views.edit_profile, name='Edit Profile'),
-    path('', views.donation_form, name='Donation Form'),
     path('match/', views.alg_result, name='alg_result'),
     path('dashboard/', views.edit_user, name='edit_user'),
     path('api/', views.api, name='api')
