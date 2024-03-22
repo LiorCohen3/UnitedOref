8db9cf5fff459cb9a42571410a18a1d04e565bc2
Lotem <lotemyak@gmail.com>
2024-03-22 17:59:39 +0200

    another js fix

--- main/static/js/minimap.js
+++ main/static/js/minimap.js
@@ -1,19 +1,15 @@
 
-async function initMiniMap(latitude, longitude, mapId) {
+function initMiniMap(latitude, longitude, mapId) {
     const myLatLng = { lat: latitude, lng: longitude };
     const mapElement = document.getElementById(mapId);
+    const map = new google.maps.Map(mapElement, {
+         zoom: 12,
+         center: myLatLng
+    });
 
-    async function initializeMiniMap() {
-        const map = new google.maps.Map(mapElement, {
-            zoom: 12,
-            center: myLatLng
-        });
-
-        new google.maps.Marker({
-            map: map,
-            position: myLatLng,
-            title: 'Your Location'
-        });
-
-    initializeMiniMap();
+    new google.maps.Marker({
+        map: map,
+        position: myLatLng,
+        title: 'Your Location'
+    });
 }
