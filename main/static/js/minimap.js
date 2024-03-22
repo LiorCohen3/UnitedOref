dbd7752026097c64fe91bfedf40bb8287e681231
Lotem <lotemyak@gmail.com>
2024-03-22 17:49:18 +0200

    trying to fix the api

--- main/static/js/minimap.js
+++ main/static/js/minimap.js
@@ -1,11 +1,9 @@
 
-function initMiniMap(latitude, longitude, mapId) {
+async function initMiniMap(latitude, longitude, mapId) {
     const myLatLng = { lat: latitude, lng: longitude };
     const mapElement = document.getElementById(mapId);
 
     async function initializeMiniMap() {
-
-        async try {
         const map = new google.maps.Map(mapElement, {
             zoom: 12,
             center: myLatLng
@@ -16,10 +14,6 @@ function initMiniMap(latitude, longitude, mapId) {
             position: myLatLng,
             title: 'Your Location'
         });
-        } catch (error) {
-            console.error('Error initializing mini map:', error.message);
-        }
-    }
 
     initializeMiniMap();
 }
