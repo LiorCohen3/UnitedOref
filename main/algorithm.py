b0005cc4c2ecc90d69072b7ff8fa22dd48ab6976
Lior Cohen <lior32166@gmail.com>
2024-03-29 16:53:44 +0300

    Algorithm scores also based on schedule date

--- main/algorithm.py
+++ main/algorithm.py
@@ -3,10 +3,6 @@ from datetime import datetime
 
 def alg(requests_list, area, item_type, count, donation_type):
     alg_score = {}
-    current_date_time = datetime.now()
-    current_year = current_date_time.year
-    current_month = current_date_time.month
-    current_day = current_date_time.day
     for index, req in enumerate(requests_list):
         alg_score[index] = 0
         if req.area == area:  # same area donation
@@ -28,16 +24,33 @@ def alg(requests_list, area, item_type, count, donation_type):
 
         alg_score[index] -= req.requestor.donations_received * 5  # take into account completed requestor donations
 
-        request_date = str(req.date).split('-')
-        request_year = int(request_date[0])
-        request_month = int(request_date[1])
-        request_day = int(request_date[2])
+        current_date = datetime.now()
 
-        current_date = datetime(current_year, current_month, current_day)
-        request_date = datetime(request_year, request_month, request_day)
-        difference = current_date - request_date
-        diff_days = difference.days if difference.days < 180 else 180  # 180 days is the maximum to be scored
-        alg_score[index] += (2 * diff_days)
+        schedule_date = str(req.schedule_date).split('-')
+        schedule_year = int(schedule_date[0])
+        schedule_month = int(schedule_date[1])
+        schedule_day = int(schedule_date[2])
+        schedule_date = datetime(schedule_year, schedule_month, schedule_day)
+
+        if current_date > schedule_date:  # if the scheduled time for the request has already passed
+            request_date = str(req.date).split('-')
+            request_year = int(request_date[0])
+            request_month = int(request_date[1])
+            request_day = int(request_date[2])
+            request_date = datetime(request_year, request_month, request_day)
+
+            difference = current_date - request_date
+            diff_days = difference.days if difference.days < 180 else 180  # 180 days is the maximum to be scored
+            alg_score[index] += (2 * diff_days)
+
+        else:
+            print(f"schedule_date: {schedule_date}")
+            difference = schedule_date - current_date
+            diff_days = difference.days
+            score_by_days = 180 - diff_days if diff_days < 180 else 0  # 180 days is the maximum to be scored
+            #  the score is based on how close the scheduled date is, the smaller the difference the higher the score
+            alg_score[index] += (2 * score_by_days)
+
         print(f"req_id: {req.requests_id} area: {req.area} item_type: {req.item_type} type_id: {req.type_id}"
               f" score: {alg_score[index]}")
 
